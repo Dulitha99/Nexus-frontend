51fdca2d47266bba37db6900cc23345980e2a174
Dulitha <dulitha.wickramasinghe@ivedha.com>
2024-05-21 01:11:46 +0530

    tensorflow added

--- src/components/messages/MessageInput.jsx
+++ src/components/messages/MessageInput.jsx
@@ -1,47 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BsSend } from "react-icons/bs";
 import useSendMessage from "../../hooks/useSendMessage";
 import useConversation from "../../zustand/useConversation";
+import * as toxicity from "@tensorflow-models/toxicity";
 
 const MessageInput = () => {
   const [message, setMessage] = useState("");
+  const [toxicityModel, setToxicityModel] = useState(null);
   const { loading, sendMessage } = useSendMessage();
   const { selectedConversation } = useConversation();
 
+  useEffect(() => {
+    async function loadToxicityModel() {
+      const threshold = 0.9;
+      const model = await toxicity.load(threshold);
+      setToxicityModel(model);
+    }
+    loadToxicityModel();
+  }, []);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!message || !selectedConversation) return;
-    await sendMessage(message, selectedConversation.id);
-    setMessage("");
+    if (!message || !selectedConversation || !toxicityModel) return;
+
+    const sentences = [message];
+    const predictions = await toxicityModel.classify(sentences);
+    const isToxic = predictions[6].results[0].match;
+    console.log(predictions[6].results[0].match);
+    if (isToxic) {
+      alert("Toxic message detected! Message not sent.");
+      setMessage("");
+    } else {
+      await sendMessage(message, selectedConversation.id);
+      setMessage("");
+    }
   };
 
   return (
     <div>
       <form className="px-4 my-3" onSubmit={handleSubmit}>
-        <div className="relative w-full">
+        <div className="w-full relative">
           <input
             type="text"
-            className="border text-sm rounded-lg block w-full pl-4 pr-12 p-2.5 bg-gray-700 border-gray-600 text-white"
+            className="border text-sm rounded-lg block w-full pl-4 pr-10 p-2.5 bg-gray-700 border-gray-600 text-white"
             placeholder="Send a message"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
           />
-          <button
-            type="submit"
-            className="absolute inset-y-0 right-3 flex items-center focus:outline-none"
-            style={{
-              background: "none",
-              border: "none",
-              padding: 0,
-              margin: 0,
-            }}
-          >
+          <span className="absolute inset-y-0 right-0 flex items-center pr-3">
             {loading ? (
               <div className="loading loading-spinner"></div>
             ) : (
-              <BsSend className="w-5 h-5 text-white" />
+              <BsSend
+                className="w-5 h-5 text-white cursor-pointer"
+                onClick={handleSubmit}
+              />
             )}
-          </button>
+          </span>
         </div>
       </form>
     </div>
