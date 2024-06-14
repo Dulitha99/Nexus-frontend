68f67884755175b7ab83c7994879f29a1be075c4
Dulitha <dulitha.wickramasinghe@ivedha.com>
2024-06-14 12:04:07 +0530

    completed

--- src/components/messages/MessageInput.jsx
+++ src/components/messages/MessageInput.jsx
@@ -66,3 +66,50 @@ const MessageInput = () => {
 };
 
 export default MessageInput;
+
+// import { useState } from "react";
+// import { BsSend } from "react-icons/bs";
+// import useSendMessage from "../../hooks/useSendMessage";
+// import useConversation from "../../zustand/useConversation";
+
+// const MessageInput = () => {
+//   const [message, setMessage] = useState("");
+//   const { loading, sendMessage } = useSendMessage();
+//   const { selectedConversation } = useConversation();
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     if (!message || !selectedConversation) return;
+
+//     await sendMessage(message, selectedConversation.id);
+//     setMessage("");
+//   };
+
+//   return (
+//     <div>
+//       <form className="px-4 my-3" onSubmit={handleSubmit}>
+//         <div className="w-full relative">
+//           <input
+//             type="text"
+//             className="border text-sm rounded-lg block w-full pl-4 pr-10 p-2.5 bg-gray-700 border-gray-600 text-white"
+//             placeholder="Send a message"
+//             value={message}
+//             onChange={(e) => setMessage(e.target.value)}
+//           />
+//           <span className="absolute inset-y-0 right-0 flex items-center pr-3">
+//             {loading ? (
+//               <div className="loading loading-spinner"></div>
+//             ) : (
+//               <BsSend
+//                 className="w-5 h-5 text-white cursor-pointer"
+//                 onClick={handleSubmit}
+//               />
+//             )}
+//           </span>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default MessageInput;
